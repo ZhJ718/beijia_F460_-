1a3608fe710304ed9035e432f117d0b4c853bc34
zhujie <2603033350@qq.com>
2023-12-07 16:00:03 +0800

    删除所有中间文件

--- workbench/HC32F460/Master/Objects/sort.d
+++
@@ -1,3 +0,0 @@
-.\objects\sort.o: ..\..\..\include\sort.c
-.\objects\sort.o: ..\..\..\include\sort.h
-.\objects\sort.o: D:\SoftWare\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- workbench/HC32F460/Master/Objects/startup_hc32f46x.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_hc32f46x.o: ..\..\..\board\HC32F460\core\startup_hc32f46x.s
